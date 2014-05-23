d913df7e7e93fd839518a12150dcecb097b2f0d1
Jana <vidrih.jana@gmail.com>
2014-05-23 12:53:35 +0200

    Vsak zacetek je tezak.

---
+++ Urejanje_z_vstavljanjem.v
@@ -0,0 +1,8 @@
+(** UREJANJE Z VSTAVLJANJEM 
+Projekt Barbare Bajcer in Jane Vidrih.
+Priporočava, da ne bereš dalje :) *)
+
+Require Import List.
+Require Import Bool.
+Require Import ZArith.
+Require Import sort.
