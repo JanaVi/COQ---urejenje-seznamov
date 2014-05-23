afb6e5c1f8076e82fa2396dead709c98cf3502ab
Jana <vidrih.jana@gmail.com>
2014-05-23 13:58:46 +0200

    Potrjeno, da algoritem z vstavljanjem deluje, pod predpostavko, da deluje vstavi.

--- Urejanje_z_vstavljanjem.v
+++ Urejanje_z_vstavljanjem.v
@@ -6,3 +6,38 @@ Require Import List.
 Require Import Bool.
 Require Import ZArith.
 Require Import sort.
+
+SearchAbout (Z.leb).
+
+Function vstavi (x : Z) (l : list Z) :=
+  match l with
+   | nil => x :: nil
+   | y :: l' => 
+     if Z.leb x y then (x :: y :: l') else y :: vstavi x l'
+  end.
+
+
+Function insertion (l :list Z) := 
+  match l with
+    | nil => nil
+    | x :: l' => 
+        let l'' := insertion l' in vstavi x l''
+  end.
+
+Eval compute in (insertion(2::3::1::15::3999::8::146::nil)%Z).
+
+Lemma vstavi_deluje (x : Z) (l : list Z) : urejen ( vstavi x l).
+Proof.
+  admit.
+Qed.
+
+
+Theorem AlgoritemDeluje (l: list Z):
+urejen (insertion l).
+Proof.
+  apply (list_ind_2 (fun l => urejen (insertion l))) ; simpl ; auto.
+  intros.
+  apply vstavi_deluje.
+Qed.
+  
+ 
